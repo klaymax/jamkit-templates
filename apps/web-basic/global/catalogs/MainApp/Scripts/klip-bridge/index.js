01052cc2491d22b84aa1e6faa2eef3bd3d3d9960
Hanyeol Cho <hanyeol.cho@gmail.com>
2022-03-18 22:20:28 +0900

    Fix the issue for undefined options

--- apps/web-basic/global/catalogs/MainApp/Scripts/klip-bridge/index.js
+++ apps/web-basic/global/catalogs/MainApp/Scripts/klip-bridge/index.js
@@ -157,7 +157,7 @@ var module = (function() {
 
     global["klip_fetch_request"] = function(params) {
         var { url, options } = JSON.parse(params["params"]);
-        var [ command, cmd_params ] = _parse_klip_command(url, options);
+        var [ command, cmd_params ] = _parse_klip_command(url, options || {});
 
         _handle_klip_command(command, cmd_params)
             .then(function(result) {
